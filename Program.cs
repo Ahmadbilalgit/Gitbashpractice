6e69f1a8428a736fd8a2fe5fc4145c34afa8f8db
Ahmed Bilal <ahmed.bilal@netrevelation.com>
2020-01-01 16:56:52 +0500

    First Commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿static class First
+
+{
+
+    public static void Sumup()
+    {
+        int x = 60;
+        System.Console.WriteLine(x);
+
+
+    }
+
+}
+
+class Aplpha
+{
+
+    public static void Main()
+    {
+        First.Sumup();   //Since the first class is defined as static , it does not require object to user its method.
+
+
+
+
+
+    }
+
+
+}
